38ecb58c701070381bf55a147650dc3bf70fae91
xapr07001 <xapr07001@utb.vaxjo.se>
2025-04-28 09:43:29 +0200

    .

--- obj/Debug/net8.0-windows/monogame.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/monogame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("monogame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0d0eee59f7925c7159fa9c1e552a019b77783b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08f1d1231169f678aac4f529d2d942148d3c3390")]
 [assembly: System.Reflection.AssemblyProductAttribute("monogame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("monogame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
